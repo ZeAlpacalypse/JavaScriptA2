ce0436aaee4a674a5030c389042cc147caad2bdd
Will Fox <foxwilliam358@gmail.com>
2023-01-31 13:40:00 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,5 +1,15 @@
-//On load we make the multiple tabs that will open and hide our other page sections
+//On load we select the container holding the tabs,
+//then listen for the taab to be clicked
+//then select all the tabs that were clicked, which is only one 
 window.onload = function MakingTabs()
 {
-
+    document
+        .querySelector('#tabContainer')
+        .addEventListener('click', TabClickEvent);
+    document.querySelectorAll('.tab')[0].click();
 };
+//Adding the click event for the tabs
+function TabClickEvent(evt)
+{
+ console.log('place holder');
+}
